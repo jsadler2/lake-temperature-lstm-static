eff3c8f1289d4d6425e520c0ff10d8a479ce3a03
Andy McAliley <wmcaliley@usgs.gov>
2022-01-06 19:22:27 -0700

    Use download path to figure out SB ID
    This does allow downloading in parallel
    The downside is that one folder = one SB ID

--- 1_fetch/src/sb_fetch.py
+++ 1_fetch/src/sb_fetch.py
@@ -1,7 +1,8 @@
+# Download ScienceBase items by item id, and optionally by file name
+
 import os
 import sys
 import argparse
-import logging
 from sciencebasepy import SbSession
 
 def sb_get(sb_session, item_id, sb_data_file=None, destination_dir='.'):
@@ -32,35 +33,39 @@ def sb_get(sb_session, item_id, sb_data_file=None, destination_dir='.'):
                 file_found = True
                 response = sb_session.download_file(file_info['url'], file_info['name'], destination_dir)
         if not file_found:
-            raise FileNotFoundError(f'{sb_data_file} not found')
+            raise FileNotFoundError(f'{sb_data_file} not found on ScienceBase')
 
     return response
 
 
-def main(item_id, filename=None, destination_dir='.', log_file='1_fetch/log/fetch.log'):
+def main(item_id, filepath=None):
     """
     Download a ScienceBase item
 
     :param item_id: ScienceBase ID of item to download
-    :param filename: Name of file to download. If None, download all files. (Default value = None)
-    :param destination_dir: directory to save to (Default value = '.')
+    :param filepath: Name of file to download, and path to download to.
+        If None, download all files. (Default value = None)
     :returns: ScienceBase JSON response
 
     """
-    if not os.path.exists(destination_dir):
-        os.makedirs(destination_dir)
-    sb = SbSession()
-    if filename is None:
+    # for a default None filepath, download all files in the item
+    if filepath is None:
+        filename = None
+        destination_dir='.'
         item_str = item_id
     else:
-        item_str = filename
-    print(f'Getting {item_str}, to {destination_dir}')
+        filename = os.path.basename(filepath)
+        destination_dir = os.path.dirname(filepath)
+        if not os.path.exists(destination_dir):
+            os.makedirs(destination_dir)
+        if filename == '':
+            filename = None
+            item_str = item_id
+        else:
+            item_str = filename
+    sb = SbSession()
+    print(f'Downloading {item_str} to {destination_dir}')
     response = sb_get(sb, item_id, filename, destination_dir=destination_dir)
-    logging.basicConfig(
-        filename=log_file,
-        encoding='utf-8',
-        level=logging.DEBUG)
-    logging.info(response)
 
 
 def parse_args():
@@ -72,14 +77,9 @@ def parse_args():
         help='ScienceBase item ID')
     parser.add_argument(
         '-f',
-        '--filename',
+        '--filepath',
         type=str,
-        help='Name of file to download from ScienceBase')
-    parser.add_argument(
-        '-d',
-        '--destination_dir',
-        type=str,
-        help='Directory to download to')
+        help='File to download from ScienceBase, and path to download to')
     args = parser.parse_args()
     arguments = (args.item_id,)
     keyword_arguments = {k:v for k,v in vars(args).items() if ((v is not None) and (k!='item_id'))}
--- Snakefile
+++ Snakefile
@@ -1,63 +1,37 @@
-# import os
-# import sys
-# # bad smell...
-# path_1_fetch = os.path.abspath(os.path.join(__file__, '1_fetch', 'src'))
-# print(path_1_fetch)
-# if path_1_fetch not in sys.path:
-#     sys.path.append(path_1_fetch)
-# from sb_fetch import sb_get
+configfile: "1_fetch/fetch_config.yaml"
+
 
 rule fetch_all:
     input:
         "1_fetch/in/pull_date.txt",
-        "1_fetch/out/lake_metadata.csv",
-        "1_fetch/out/obs/temperature_observations.zip",
-        # "1_fetch/out/driver/04_inputs.xml"
-        "1_fetch/out/driver/clarity_01_N48.00-49.50_W89.50-97.25.zip"
+        expand("{file}", file=config["sb_fetch.py"]["metadata_files"]),
+        expand("{file}", file=config["sb_fetch.py"]["obs_files"]),
+        expand("{file}", file=config["sb_fetch.py"]["driver_files"])
 
-# 1: Lake information for 881 lakes
+# Lake information for 881 MNTOHA lakes
 rule fetch_metadata:
     input:
         "1_fetch/in/pull_date.txt"
     output:
-        "1_fetch/out/lake_metadata.csv"
+        "1_fetch/out/metadata/{file}"
     shell:
-        "python 1_fetch/src/sb_fetch.py '5e5c1c1ce4b01d50924f27e7' -f lake_metadata.csv -d '1_fetch/out'"
+        "python 1_fetch/src/sb_fetch.py {config[sb_fetch.py][metadata_id]} -f {output}"
 
-# 2: Water temperature observations
+# Water temperature observations
 rule fetch_obs:
     input:
         "1_fetch/in/pull_date.txt"
     output:
-        "1_fetch/out/obs/temperature_observations.zip"
+        "1_fetch/out/obs/{file}"
     shell:
-        "python 1_fetch/src/sb_fetch.py '5e5d0b68e4b01d50924f2b32' -f temperature_observations.zip -d '1_fetch/out/obs'"
+        "python 1_fetch/src/sb_fetch.py {config[sb_fetch.py][obs_id]} -f {output}"
 
-# 3: Model configurations (lake model parameter values)
-# rule fetch_pb0_config:
-#     input:
-#         "1_fetch/in/pull_date.txt"
-#     output:
-#         "1_fetch/out/pb0_config.json"
-#     shell:
-#         "python 1_fetch/src/sb_fetch.py '5e5c1c36e4b01d50924f27ea' -d '1_fetch/out'"
-
-# 4: Model inputs (meteorological inputs, clarity, and ice flags)
-rule fetch_drivers:
+# Model inputs (meteorological inputs, clarity, and ice flags)
+rule fetch_driver:
     input:
         "1_fetch/in/pull_date.txt"
     output:
-        # "1_fetch/out/driver/04_inputs.xml"
-        "1_fetch/out/driver/clarity_01_N48.00-49.50_W89.50-97.25.zip"
+        "1_fetch/out/driver/{file}"
     shell:
-        "python 1_fetch/src/sb_fetch.py '5e5d0b96e4b01d50924f2b34' -d '1_fetch/out/driver'"
-
-# 5: Model prediction data
-# rule fetch_glm_preds:
-#     input:
-#         "1_fetch/in/pull_date.txt"
-#     output:
-#         "1_fetch/out/05_predictions.xml"
-#     shell:
-#         "python 1_fetch/src/sb_fetch.py '5e5d0bb9e4b01d50924f2b36' -d '1_fetch/out'"
+        "python 1_fetch/src/sb_fetch.py {config[sb_fetch.py][driver_id]} -f {output}"
 
